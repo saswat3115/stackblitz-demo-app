7bde2ecd9d5a46bf72c9e418f40c5f14d2020e89
saswat3115 <saswat3115@gmail.com>
2021-06-03 11:12:46 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Hello StackBlitz demo app!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
